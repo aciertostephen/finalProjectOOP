745cf3272f58c534b6ce0243d248b0526f6fa215
Stephen Acierto <acierto_stephenshane@plpasig.edu.ph>
2026-09-23 16:31:05 +0800

    Update VB.NET project

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,16 +22,53 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        SplitContainer1 = New SplitContainer()
+        pnlLogin = New Panel()
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).BeginInit()
+        SplitContainer1.SuspendLayout()
+        pnlLogin.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' SplitContainer1
+        ' 
+        SplitContainer1.Dock = DockStyle.Fill
+        SplitContainer1.IsSplitterFixed = True
+        SplitContainer1.Location = New Point(0, 0)
+        SplitContainer1.Name = "SplitContainer1"
+        SplitContainer1.Size = New Size(1924, 1055)
+        SplitContainer1.SplitterDistance = 1120
+        SplitContainer1.SplitterWidth = 1
+        SplitContainer1.TabIndex = 0
+        ' 
+        ' pnlLogin
+        ' 
+        pnlLogin.Controls.Add(SplitContainer1)
+        pnlLogin.Dock = DockStyle.Fill
+        pnlLogin.Location = New Point(0, 0)
+        pnlLogin.Name = "pnlLogin"
+        pnlLogin.Size = New Size(1924, 1055)
+        pnlLogin.TabIndex = 0
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1924, 1055)
+        Controls.Add(pnlLogin)
+        FormBorderStyle = FormBorderStyle.None
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
         Name = "Form1"
-        Text = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "ELEMENT - GRADING SYSTEM"
+        WindowState = FormWindowState.Maximized
+        CType(SplitContainer1, ComponentModel.ISupportInitialize).EndInit()
+        SplitContainer1.ResumeLayout(False)
+        pnlLogin.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents pnlLogin As Panel
 
 End Class
